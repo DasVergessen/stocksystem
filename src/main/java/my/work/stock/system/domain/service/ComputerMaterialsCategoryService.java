310bc256dcdca5a6fed7f5d38206ccc66e6e24f6
wangdengwu <dengwu.wang@gmail.com>
2017-04-03 10:50:02 +0800

    innerdepartment

---
+++ src/main/java/my/work/stock/system/domain/service/ComputerMaterialsCategoryService.java
@@ -0,0 +1,7 @@
+package my.work.stock.system.domain.service;
+
+/**
+ * Created by wangdengwu on 2017/4/3.
+ */
+public class ComputerMaterialsCategoryService {
+}
---
+++ src/main/java/my/work/stock/system/web/view/SearchComputerMaterialsCategory.java
@@ -0,0 +1,7 @@
+package my.work.stock.system.web.view;
+
+/**
+ * Created by wangdengwu on 2017/4/3.
+ */
+public class SearchComputerMaterialsCategory {
+}
