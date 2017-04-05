bf1956e806f1eb10671abde7e72fba9fac990237
wangdengwu <dengwu.wang@gmail.com>
2017-04-05 17:30:00 +0800

    add start data

---
+++ src/main/java/my/work/stock/system/MyApplicationStartedEventListener.java
@@ -0,0 +1,20 @@
+package my.work.stock.system;
+
+import my.work.stock.system.domain.entity.UserInfo;
+import my.work.stock.system.domain.service.UserInfoService;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class MyApplicationStartedEventListener implements ApplicationListener<ContextRefreshedEvent> {
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        UserInfoService userInfoService = contextRefreshedEvent.getApplicationContext().getBean(UserInfoService.class);
+        UserInfo admin = userInfoService.findByUserName("admin");
+        if (admin == null) {
+            UserInfo userInfo = new UserInfo();
+            userInfo.setUserName("admin");
+            userInfo.setPassword("123456");
+            userInfoService.save(userInfo);
+        }
+    }
+}
