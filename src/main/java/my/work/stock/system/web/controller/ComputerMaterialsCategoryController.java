c8213019630356d0b3aa1b737244bd196fc488d0
wangdengwu <dengwu.wang@gmail.com>
2017-04-10 23:48:35 +0800

    computerMaterialsCategory

--- src/main/java/my/work/stock/system/web/controller/ComputerMaterialsCategoryController.java
+++ src/main/java/my/work/stock/system/web/controller/ComputerMaterialsCategoryController.java
@@ -3,6 +3,10 @@ package my.work.stock.system.web.controller;
 import my.work.stock.system.domain.entity.ComputerMaterialsCategory;
 import my.work.stock.system.domain.service.ComputerMaterialsCategoryService;
 import my.work.stock.system.web.view.SearchComputerMaterialsCategory;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,7 +56,29 @@ public class ComputerMaterialsCategoryController {
     }
 
     @RequestMapping(value = "export", method = RequestMethod.GET)
-    public Boolean export() {
+    public Boolean export(HttpServletResponse response) throws IOException {
+        List<ComputerMaterialsCategory> allComputerMaterialsCategory = computerMaterialsCategoryService.findAllComputerMaterialsCategory();
+        response.setContentType("text/csv;charset=GBK");
+        String headerKey = "Content-Disposition";
+        String csvFileName = "计算机耗材类别信息.xlsx";
+        String headerValue = String.format("attachment; filename=\"%s\"", new String(csvFileName.getBytes(Charset.forName("GBK")), "ISO-8859-1"));
+        response.setHeader(headerKey, headerValue);
+        Workbook wb = new XSSFWorkbook();
+        Sheet computerMaterialsCategorySheet = wb.createSheet("计算机耗材类别信息");
+
+        Row header = computerMaterialsCategorySheet.createRow((short) 0);//头
+        header.createCell(0).setCellValue("类别编码");
+        header.createCell(1).setCellValue("类别名称");
+
+        for (int i = 0; i < allComputerMaterialsCategory.size(); i++) {
+            Row computerMaterialsCategorySheetRow = computerMaterialsCategorySheet.createRow(i + 1);
+            ComputerMaterialsCategory computerMaterialsCategory = allComputerMaterialsCategory.get(i);
+            computerMaterialsCategorySheetRow.createCell(0).setCellValue(computerMaterialsCategory.getCategoryId());
+            computerMaterialsCategorySheetRow.createCell(1).setCellValue(computerMaterialsCategory.getCategoryName());
+        }
+
+        wb.write(response.getOutputStream());
+        wb.close();
         return Boolean.TRUE;
     }
 }
