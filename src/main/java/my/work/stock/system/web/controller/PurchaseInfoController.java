086c52881d820c8b248d809dbe6f5ecab8e1bd76
wangdengwu <dengwu.wang@gmail.com>
2017-04-11 11:43:39 +0800

    purchaseInfos

--- src/main/java/my/work/stock/system/web/controller/PurchaseInfoController.java
+++ src/main/java/my/work/stock/system/web/controller/PurchaseInfoController.java
@@ -3,6 +3,10 @@ package my.work.stock.system.web.controller;
 import my.work.stock.system.domain.entity.PurchaseInfo;
 import my.work.stock.system.domain.service.PurchaseInfoService;
 import my.work.stock.system.web.view.SearchPurchaseInfo;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -55,7 +63,53 @@ public class PurchaseInfoController {
     }
 
     @RequestMapping(value = "export", method = RequestMethod.GET)
-    public Boolean export() {
+    public Boolean export(HttpServletResponse response) throws IOException {
+
+        List<PurchaseInfo> allPurchaseInfo = purchaseInfoService.findAllPurchaseInfo();
+        response.setContentType("text/csv;charset=GBK");
+        String headerKey = "Content-Disposition";
+        String csvFileName = "采购入库.xlsx";
+        String headerValue = String.format("attachment; filename=\"%s\"", new String(csvFileName.getBytes(Charset.forName("GBK")), "ISO-8859-1"));
+        response.setHeader(headerKey, headerValue);
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet("采购入库");
+
+        Row header = sheet.createRow((short) 0);//头
+        header.createCell(0).setCellValue("入库单号");
+        header.createCell(1).setCellValue("入库日期");
+        header.createCell(2).setCellValue("供应商");
+        header.createCell(3).setCellValue("耗材编码");
+        header.createCell(4).setCellValue("耗材名称");
+        header.createCell(5).setCellValue("耗材规格");
+        header.createCell(6).setCellValue("所属类别");
+        header.createCell(7).setCellValue("单位");
+        header.createCell(8).setCellValue("入库数量");
+        header.createCell(9).setCellValue("单价");
+        header.createCell(10).setCellValue("合计金额");
+        header.createCell(11).setCellValue("备注信息");
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        for (int i = 0; i < allPurchaseInfo.size(); i++) {
+            Row row = sheet.createRow(i + 1);
+            PurchaseInfo purchaseInfo = allPurchaseInfo.get(i);
+            row.createCell(0).setCellValue("RK" + purchaseInfo.getPurchaseId());
+            row.createCell(1).setCellValue(purchaseInfo.getPurchaseDate());
+            row.createCell(2).setCellValue(purchaseInfo.getSupplierInfo().getSupplierName());
+            row.createCell(3).setCellValue(purchaseInfo.getComputerMaterialsInfo().getComputerMaterialsId());
+            row.createCell(4).setCellValue(purchaseInfo.getComputerMaterialsInfo().getComputerMaterialsName());
+            row.createCell(5).setCellValue(purchaseInfo.getComputerMaterialsInfo().getComputerMaterialsSpecifications());
+            row.createCell(6).setCellValue(purchaseInfo.getComputerMaterialsCategory().getCategoryName());
+            row.createCell(7).setCellValue(purchaseInfo.getComputerMaterialsInfo().getComputerMaterialsUnit());
+            row.createCell(8).setCellValue(purchaseInfo.getPurchaseQuantity());
+            row.createCell(9).setCellValue(decimalFormat.format(purchaseInfo.getPurchasePrice() / 100.00));
+            row.createCell(10).setCellValue(decimalFormat.format(purchaseInfo.getPurchasePrice() * purchaseInfo.getPurchaseQuantity() / 100.00));
+            row.createCell(11).setCellValue(purchaseInfo.getMemo());
+        }
+
+        wb.write(response.getOutputStream());
+        wb.close();
+
         return Boolean.TRUE;
     }
 }
